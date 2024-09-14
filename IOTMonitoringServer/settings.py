91982092247fa8d7127e87f32888e4e4d2a0deba
migueltovar <loganitsolutions@outlook.com>
2024-09-13 22:28:27 -0500

    ajustando settings

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "52.207.248.89"]
+ALLOWED_HOSTS = ["localhost", "98.81.198.149"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.210.193.198",  # Dirección IP de la base de datos
+        "HOST": "34.239.111.119",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
