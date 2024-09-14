6e332a77e8143fc114b01d87523ba54b95f29e22
migueltovar <loganitsolutions@outlook.com>
2024-09-13 22:36:28 -0500

    cambiando settings para db

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "34.239.111.119",  # Dirección IP de la base de datos
+        "HOST": "3.93.9.162",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
